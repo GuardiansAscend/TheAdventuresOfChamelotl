48da872f304aaecc5ac353eb050336f302c9f53b
Kevin Scanlan <kevina.m.scanlan@gmail.com>
2024-11-07 14:54:41 -0500

    Character colour switching is finished

--- Chamelotl/Assets/Scripts/PlayerColor.cs
+++ Chamelotl/Assets/Scripts/PlayerColor.cs
@@ -4,17 +4,64 @@ using static Fruit;
 public class PlayerColor : MonoBehaviour
 {
     public FruitColor characterColor;
+    public Sprite playerSpriteYellow;
+    public Sprite playerSpriteRed;
+    public Sprite playerSpritePurple;
+    public Sprite playerSpriteBrown;
+    public RuntimeAnimatorController yellowAnimatorController;
+    public RuntimeAnimatorController redAnimatorController;
+    public RuntimeAnimatorController purpleAnimatorController;
+    public RuntimeAnimatorController brownAnimatorController;
 
-    void Start()
-    {
-        characterColor = FruitColor.White;
-    }
+
 
     public void ChangeColor(FruitColor newColor)
     {
         characterColor = newColor;
+        Sprite newColorSprite = null;
+        RuntimeAnimatorController newAnimatorController = null;
 
-        // Change the color of the player sprite
+        switch (newColor)
+        {
+            case FruitColor.Yellow:
+                newColorSprite = playerSpriteYellow;
+                newAnimatorController = yellowAnimatorController;
+                break;
+            case FruitColor.Red:
+                newColorSprite = playerSpriteRed;
+                newAnimatorController = redAnimatorController;
+                break;
+            case FruitColor.Purple:
+                newColorSprite = playerSpritePurple;
+                newAnimatorController = purpleAnimatorController;
+                break;
+            case FruitColor.Brown:
+                newColorSprite = playerSpriteBrown;
+                newAnimatorController = brownAnimatorController;
+                break;
+        }
+
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer != null && newColorSprite != null)
+        {
+            spriteRenderer.sprite = newColorSprite;
+        }
+        else
+        {
+            Debug.LogWarning("SpriteRenderer or newSprite is not set.");
+        }
+
+        Animator animator = GetComponent<Animator>();
+
+        if (animator != null && newAnimatorController != null)
+        {
+            animator.runtimeAnimatorController = newAnimatorController;
+        }
+        else
+        {
+            Debug.LogWarning("Animator or newAnimatorController is not set.");
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
